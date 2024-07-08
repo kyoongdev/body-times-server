6afdc8e6ce7216c7559b459e459c4199b36b6ffb
kyoongdev <9898junjun2@gmail.com>
2024-07-08 16:40:28 +0900

    feat(swagger): swagger setting

--- src/main/java/kyoongdev/body_times/BodyTimesApplication.java
+++ src/main/java/kyoongdev/body_times/BodyTimesApplication.java
@@ -2,8 +2,9 @@ package kyoongdev.body_times;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BodyTimesApplication {
 
 	public static void main(String[] args) {
