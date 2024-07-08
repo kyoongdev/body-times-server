95a6f9a9ae20c104e4197849a272ffc7d56bb09f
kyoongdev <9898junjun2@gmail.com>
2024-07-08 16:14:05 +0900

    feat(init): init

---
+++ src/main/java/kyoongdev/body_times/BodyTimesApplication.java
@@ -0,0 +1,13 @@
+package kyoongdev.body_times;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodyTimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodyTimesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kyoongdev/body_times/BodyTimesApplicationTests.java
@@ -0,0 +1,13 @@
+package kyoongdev.body_times;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodyTimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
