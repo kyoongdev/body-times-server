7d67c81eb588dd504821a86524e26f8e68afed49
kyoongdev <9898junjun2@gmail.com>
2024-07-19 17:29:57 +0900

    feat(auth): social login with tokens

--- src/main/java/kyoongdev/body_times/config/SecurityConfig.java
+++ src/main/java/kyoongdev/body_times/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package kyoongdev.body_times.config;
 
 
+import kyoongdev.body_times.config.jwt.JwtFilter;
+import kyoongdev.body_times.config.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +11,19 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+  private final JwtProvider jwtProvider;
 
   @Bean
   public AuthenticationManager authenticationManager(
@@ -27,41 +35,15 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-//    http
-//        .csrf((csrfConfig) ->
-//            csrfConfig.disable()
-//        )
-//        .headers((headerConfig) ->
-//            headerConfig.frameOptions(frameOptionsConfig ->
-//                frameOptionsConfig.disable()
-//            )
-//        ).apply()
+    http
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(AbstractHttpConfigurer::disable)
+        .sessionManagement(m -> m.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .formLogin(AbstractHttpConfigurer::disable)
+        .httpBasic(AbstractHttpConfigurer::disable)
+        .headers((headerConfig) -> headerConfig.frameOptions(FrameOptionsConfig::disable)
+        ).addFilterBefore(new JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
 
-//    http
-//        .httpBasic().disable()  // 비인증시 login form redirect X (rest api)
-//        .csrf().disable()       // crsf 보안 X (rest api)
-//        .sessionManagement()
-//        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt token으로 인증 > 세션 필요없음
-//
-//        .and()
-//        .authorizeRequests()    // 다음 리퀘스트에 대한 사용권한 체크
-//        .antMatchers("/**").permitAll() // 모든 주소 허용
-//        .anyRequest().authenticated() // Authentication 필요한 주소
-//
-//        .and()                  // exception handling for jwt
-//        .exceptionHandling()
-////        .accessDeniedHandler(jwtAccessDeniedHandler)
-////        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-//
-//        .and()
-//        .oauth2Login()
-//        .successHandler(oAuth2LoginSuccessHandler)
-//        .failureHandler(oAuth2LoginFailureHandler)
-//        .userInfoEndpoint().userService(customOAuth2UserService);
-//    ;
-//
-//    // jwt 적용
-//    http.apply(new JwtSecurityConfig(jwtTokenProvider));
     return http.build();
   }
 
--- src/main/java/kyoongdev/body_times/modules/auth/AuthService.java
+++ src/main/java/kyoongdev/body_times/modules/auth/AuthService.java
@@ -2,15 +2,20 @@ package kyoongdev.body_times.modules.auth;
 
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import kyoongdev.body_times.config.jwt.JwtProvider;
+import kyoongdev.body_times.modules.auth.dto.TokenDTO;
 import kyoongdev.body_times.modules.user.UserRepository;
 import kyoongdev.body_times.modules.user.dto.CreateSocialUserDTO;
+import kyoongdev.body_times.modules.user.entities.User;
 import kyoongdev.body_times.utils.social.KakaoSocial;
 import kyoongdev.body_times.utils.social.dto.KakaoUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
 
-@Component
+@Service
 @Slf4j
 @RequiredArgsConstructor
 public class AuthService {
@@ -18,6 +23,11 @@ public class AuthService {
   private final KakaoSocial kakaoSocial;
 
   private final UserRepository userRepository;
+
+  private final JwtProvider jwtProvider;
+
+  @Value("${client_url}")
+  private String clientUrl;
 
 
   public void kakaoLogin(HttpServletResponse response) throws IOException {
@@ -27,9 +37,17 @@ public class AuthService {
   public void kakaoCallback(String code, HttpServletResponse response) throws IOException {
     KakaoUser user = kakaoSocial.getRestCallback(code).getUser();
 
-    userRepository.save(CreateSocialUserDTO.toEntity(user));
+    User newUser = userRepository.save(CreateSocialUserDTO.toEntity(user));
 
-    response.sendRedirect("http://localhost:3000");
+    TokenDTO token = jwtProvider.createToken(
+        jwtProvider.getAuthentication(newUser.getId().toString()));
+
+    String redirectUri = UriComponentsBuilder.fromUriString(clientUrl)
+        .queryParam("accessToken", token.getAccessToken())
+        .queryParam("refreshToken", token.getRefreshToken())
+        .build().toUriString();
+    
+    response.sendRedirect(redirectUri);
 
   }
 
